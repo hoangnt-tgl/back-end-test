d6c97427cbab1781a093f6865b873a612fb6aaf0
Nguyen Trong Hoang <tronghoang1907@gmail.com>
2022-09-29 16:32:38 +0700

    1

--- connect.js
+++ connect.js
@@ -2,12 +2,11 @@ var mysql = require('mysql')
 const HOST = process.env.HOST
 const USER = process.env.USER
 const PASS = process.env.PASS
-const DATABASE = process.env.DATABASE
 var conn = mysql.createConnection({
-    host: HOST,
-    user: USER,
-    password: PASS,
-    database: DATABASE
+    host: "us-cdbr-east-06.cleardb.net",
+    user: "b31dd452c4bb27",
+    password: "fb488890",
+    database: "heroku_6965ea3352c823d"
 });
 conn.connect(function(err) {
     if (err) throw err;
--- models/student.js
+++ models/student.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var connect = require('../connect')
+
 module.exports.getPointByStudentId = function(id, callback){
     var sql = `SELECT name, math, english, national_language, year, class from students s INNER JOIN transcripts t ON s.id = t.student_id WHERE s.id = ${id} ORDER BY year DESC`
     connect.query(sql, callback);
