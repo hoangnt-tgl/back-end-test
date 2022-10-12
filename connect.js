d04f295e04e559098162b6725fbd4358a828c5a3
Nguyen Trong Hoang <tronghoang1907@gmail.com>
2022-10-12 17:13:31 +0700

    1

--- connect.js
+++ connect.js
@@ -1,29 +1,37 @@
 
 var mysql = require('mysql')
 
-const HOST = process.env.HOST
-const USER = process.env.USER
-const PASS = process.env.PASS
-const DATABASE = process.env.DATABASE
-var conn = mysql.createConnection({
-    // host: HOST,
-    // user: USER,
-    // password: PASS,
-    // database: DATABASE,
-    host: "us-cdbr-east-06.cleardb.net",
-    user: "b31dd452c4bb27",
-    password: "fb488890",
-    database: "heroku_6965ea3352c823d"
-});
+// const HOST = process.env.HOST 
+// const USER = process.env.USER 
+// const PASS = process.env.PASS 
+// const DATABASE = process.env.DATABASE
+
+const HOST = "us-cdbr-east-06.cleardb.net"
+const USER = "b31dd452c4bb27"
+const PASS = "fb488890"
+const DATABASE = "heroku_6965ea3352c823d"
+
+var db_config = {
+    host: HOST,
+    user: USER,
+    password: PASS,
+    database: DATABASE
+}
+
+var conn;
+
 function handleConnect() {
+    conn = mysql.createConnection(db_config);
+
     conn.connect(function (err) {
         if (err) {
             console.log('Database connection failed')
             setTimeout(handleConnect, 2000);
+        } else {
+            console.log('Database connection successful')
         }
     })
     conn.on('error', function (err) {
-        console.log('db error', err);
         if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
             handleConnect();                         // lost due to either server restart, or a
         } else {                                      // connnection idle timeout (the wait_timeout
